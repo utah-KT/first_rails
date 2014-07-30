852c35c140e115d366dae7d6768427873f24af6c
Kaito Tokumori <k148574@ie.u-ryukyu.ac.jp>
2014-07-30 22:12:41 +0900

    add secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,4 +61,17 @@ RSpec.describe User, :type => :model do
     it { should_not be_valid }
   end
 
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
 end
